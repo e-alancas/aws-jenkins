2f420316c2807e10a823570b3c56549572ddc23c
Eliecer Alan <eliecer.alan@vml.com>
2024-03-17 16:36:29 -0600

    Added dns and graphs

--- src/alb.tf
+++ src/alb.tf
@@ -40,8 +40,26 @@ resource "aws_lb_listener" "jenkins-listener-http" {
   port              = "80"
   protocol          = "HTTP"
   default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# ALB Listener for HTTPS
+resource "aws_lb_listener" "jenkins-listener-https" {
+  provider          = aws.region-master
+  load_balancer_arn = aws_lb.application-lb.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.jenkins-lb-https.arn
+  default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app-lb-tg.id
+    target_group_arn = aws_lb_target_group.app-lb-tg.arn
   }
 }
 
--- src/outputs.tf
+++ src/outputs.tf
@@ -2,6 +2,23 @@ output "Jenkins-Main-Node-Public-IP" {
   value = aws_instance.jenkins-master.public_ip
 }
 
+output "LB-DNS-NAME" {
+  value = aws_lb.application-lb.dns_name
+}
+
+output "amiId-us-east-1" {
+  value     = data.aws_ssm_parameter.linuxAmi.value
+  sensitive = true
+}
+
+output "amiId-us-west-2" {
+  value     = data.aws_ssm_parameter.linuxAmiOregon.value
+  sensitive = true
+}
+
+output "Jenkins-Main-Node-Private-IP" {
+  value = aws_instance.jenkins-master.private_ip
+}
 output "Jenkins-Worker-Public-IPs" {
   value = {
     for instance in aws_instance.jenkins-worker-oregon :
@@ -9,6 +26,6 @@ output "Jenkins-Worker-Public-IPs" {
   }
 }
 
-output "LB-DNS-NAME" {
-  value = aws_lb.application-lb.dns_name
+output "url" {
+  value = aws_route53_record.jenkins.fqdn
 }
